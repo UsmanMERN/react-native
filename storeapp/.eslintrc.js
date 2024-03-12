d7c53e433355540c1772ba8962d898d4a42f0d39
UsmanMern <mu1363131@gmail.com>
2024-03-13 01:13:23 +0500

    working

--- storeapp/.eslintrc.js
+++ storeapp/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rule: {
+    "prettier/prettier": 0,
+    'padded-blocks': 'off',
+  }
 };
---
+++ storeapp/src/components/ProductItems.tsx
@@ -0,0 +1,93 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { PropsWithChildren } from 'react'
+
+type ProductProps = PropsWithChildren<{
+    product: Product
+}>
+
+export default function ProductItems({ product }: ProductProps) {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: product.imageUrl }} style={styles.image} />
+            <View >
+                <Text style={styles.name}>{product.name}</Text>
+            </View>
+            <View style={[styles.rowContainer, styles.ratingContainer]}>
+                <View style={styles.rating}>
+                    <Text style={styles.ratingText}>{product.rating}&#9733;</Text>
+                </View>
+                <Text style={styles.ratingCount}>{product.ratingCount.toLocaleString()}</Text>
+            </View>
+            <View style={[styles.rowContainer, styles.ratingContainer]}>
+                <Text style={styles.originalPrice}>pkr {product.originalPrice.toLocaleString()}</Text>
+                <Text style={styles.discountPrice}>pkr {product.discountPrice.toLocaleString()}</Text>
+                <Text style={styles.offerPercentage}>% {product.offerPercentage.toLocaleString()} off</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 8,
+        flexDirection: 'row',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+    },
+    image: {
+        width: 90,
+        height: 150,
+        resizeMode: 'contain',
+
+        marginRight: 12,
+    },
+    name: {
+        marginBottom: 4,
+
+        fontSize: 15,
+        fontWeight: '500',
+    },
+    ratingContainer: {
+        marginBottom: 8,
+    },
+    priceContainer: {
+        marginBottom: 12,
+    },
+    rating: {
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        justifyContent: 'center',
+        backgroundColor: '#008c00',
+
+        marginRight: 4,
+    },
+    ratingText: {
+        color: '#fff',
+        fontSize: 12,
+        fontWeight: '600',
+    },
+    ratingCount: {
+        color: '#878787',
+    },
+    originalPrice: {
+        fontSize: 18,
+        marginRight: 4,
+        fontWeight: '600',
+
+        color: 'rgba(0, 0, 0, 0.5)',
+        textDecorationLine: 'line-through',
+    },
+    discountPrice: {
+        fontSize: 18,
+        marginRight: 4,
+        fontWeight: '600',
+
+        color: '#000000',
+    },
+    offerPercentage: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#4bb550',
+    },
+});
