0d27c613a0c0f2f1a5d06266254473203afae187
UsmanMern <mu1363131@gmail.com>
2024-03-14 00:00:32 +0500

    store app

--- storeapp/src/screens/Home.tsx
+++ storeapp/src/screens/Home.tsx
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackPramList } from '../App'
+
+import ProductItems from '../components/ProductItems'
+import Separator from '../components/Separator'
+
+// Data
+import { PRODUCTS_LIST } from '../data/constants'
+
+type HomeProps = NativeStackScreenProps<RootStackPramList, "Home">
 export default function Home() {
     return (
         <View>
